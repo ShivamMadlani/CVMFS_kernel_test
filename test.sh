94580216e31e4f9a608591fc8d0d060191e94fee
shivam <shivammadlani5@gmail.com>
2025-07-18 01:04:52 +0530

    updated paths

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eu
 
 REPO="big.file.test"
-FILE="/cvmfs/$REPO/big_file.img"
+FILE="/mnt/test/cvmfs/$REPO/big_file.img"
 
 echo "1. Read big_file.img from cvmfs_server"
 ./read_test "$FILE"
