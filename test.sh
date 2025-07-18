6fb4e9743245f8ab307ea396c2c5819f29d71cac
shivam <shivammadlani5@gmail.com>
2025-07-18 18:44:15 +0530

    updated path

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -eu
 
 REPO="big.file.test"
-FILE="/mnt/test/cvmfs/$REPO/big_file.img"
+FILE="/mnt/test/overlayfs-test/merged/big_file.img"
 
 echo "1. Read big_file.img from cvmfs_server"
 ./read_test "$FILE"
